ed42fe4ba3fdd33cf295f7591f06c761ac71ef09
oleksandra_stepanova <oleksandra.stepanova@gmail.com>
2024-12-05 11:20:41 +0100

    Dynamic Routing

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-xl">Dashboard page</h1>
+    </main>
+  );
+}
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-xl">Settings page</h1>
+    </main>
+  );
+}
